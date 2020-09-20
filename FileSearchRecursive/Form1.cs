1c9559aca141693d5e5808ec4988d83e779c64de
salut.lespotes@gmail.com <bruno185>
2020-09-21 00:15:59 +0200

    recursive V2

--- FileSearchRecursive/Form1.cs
+++ FileSearchRecursive/Form1.cs
@@ -3,10 +3,20 @@ using System.Collections.Generic;
 using System.ComponentModel;
 using System.Data;
 using System.Drawing;
+using System.IO;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
+
+/* 
+ * Recherche de fichier de manière récusive
+ * https://support.microsoft.com/fr-fr/help/303974.
+ * ouverture de chaque fichier
+ * recherche d'une chaine dans les fichiers
+ * comptage trouvés / pas trouvés
+ * 
+ */
 
 namespace FileSearchRecursive
 {
@@ -15,6 +25,104 @@ namespace FileSearchRecursive
         public Form1()
         {
             InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            listBox1.Items.Clear();
+            listBox1.HorizontalScrollbar = true;
+
+            DirSearch2(textBox2.Text);
+            System.Console.WriteLine(listBox1.Items[0]);
+
+        }
+
+        void DirSearch(string sDir)
+        // https://docs.microsoft.com/fr-fr/dotnet/api/system.io.file.exists?view=netcore-3.1
+        // pas mal mais ne charche pas dans le répertiore de départ.
+        {
+            try
+            {
+                foreach (string d in Directory.GetDirectories(sDir))
+                {
+                    foreach (string f in Directory.GetFiles(d, textBox1.Text))
+                    {
+                        listBox1.Items.Add(f);
+                    }
+                    DirSearch(d);
+                }
+            }
+            catch (System.Exception excpt)
+            {
+                Console.WriteLine(excpt.Message);
+            }
+        }
+
+
+        void DirSearch2(string sDir)
+            // Amélioré : commence la recherche dans le répertoire courant
+            // puis traite les sous-répertoires.
+        {
+            try
+            {
+                foreach (string f in Directory.GetFiles(sDir, textBox1.Text))
+                {
+                    listBox1.Items.Add(f);
+                }
+                foreach (string d in Directory.GetDirectories(sDir))
+                {
+                    DirSearch2(d);
+                }
+            }
+            catch (System.Exception excpt)
+            {
+                Console.WriteLine(excpt.Message);
+            }
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            string line;
+            int elements;
+            Boolean trouve;
+            int nbtrouve = 0;
+            int nbnottrouve = 0;
+            string curfile; 
+
+
+            elements = listBox1.Items.Count;
+
+            for (int i = 0; i < elements; i++)
+            {
+                trouve = false;
+                curfile = listBox1.Items[i].ToString();
+                if (File.Exists(curfile))
+                {
+                    
+                    System.IO.StreamReader File = new System.IO.StreamReader(curfile);
+                    while ((line = File.ReadLine()) != null)
+                    {
+                        //System.Console.WriteLine(line);
+                        if (line.Contains("NOTE Confidence:")) {
+                            trouve = true;
+
+                        }
+                    }
+                }
+                if (trouve)
+                {
+                    Console.WriteLine(curfile);
+                    nbtrouve = nbtrouve + 1;
+                }
+                else
+                {
+                    Console.WriteLine("Not found");
+                    nbnottrouve = nbnottrouve + 1;
+                }
+
+            }
+            Console.WriteLine("FIN " + nbtrouve.ToString());
+            Console.WriteLine("FIN " + nbnottrouve.ToString());
         }
     }
 }
